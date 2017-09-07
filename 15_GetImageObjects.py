842672496f514b88a96f9d74f119a30cb527512a
jacobwang123 <yage.wang@rhsmith.umd.edu>
2017-09-06 22:33:06 -0400

    15

--- 15_GetImageObjects.py
+++ 15_GetImageObjects.py
@@ -3,6 +3,9 @@ import sys
 import math
 import re
 from glob import glob
+import time
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 
 import argparse
 import base64
@@ -15,6 +18,7 @@ from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
 cwd = os.path.dirname(os.path.realpath(__file__))
+img_dir = os.path.join(cwd, 'Board_Image')
 
 DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
 credentials = GoogleCredentials.get_application_default()
@@ -22,43 +26,46 @@ service = discovery.build('vision', 'v1', credentials=credentials, discoveryServ
 
 lmtzr = WordNetLemmatizer()
 
-def get_top_objects(photo_dir, fnames):
-	label_batch_request = []
-	for f in fnames:
-		filename = os.path.join(photo_dir, f)
-		with open(filename,'rb') as img:
-			image_content = base64.b64encode(img.read())
-			label_batch_request.append({
-				'image': {
-					'content': image_content.decode('UTF-8')
-				},
-				'features': [{
-					'type': 'LABEL_DETECTION',
-					'maxResults': 10
-				}]
-			})
+def get_top_objects(fname):
+	with open(fname, 'rb') as img:
+		image_content = base64.b64encode(img.read())
+		# label_batch_request.append()
+		label_request = {
+			'image': {
+				'content': image_content.decode('UTF-8')
+			},
+			'features': [{
+				'type': 'LABEL_DETECTION',
+				'maxResults': 10
+			}]
+		}
 
-	label_request = service.images().annotate(body={'requests': label_batch_request})
-	label_responses = label_request.execute(num_retries=10)
-	responses = label_responses['responses']
-	
-	fh = open(os.path.join(cwd, 'Original_Image_Objects.txt'), 'a')
-	for i in range(1, len(responses)):
-		fname = fnames[i - 1]
-
-		label_output = []
-		if 'labelAnnotations' in responses[i]:
-			labels = responses[i]['labelAnnotations']
+	label_output = []
+	try:
+		label_request = service.images().annotate(body={'requests': label_request})
+		label_responses = label_request.execute(num_retries=10)
+		responses = label_responses['responses']
+		
+		if 'labelAnnotations' in responses[0]:
+			labels = responses[0]['labelAnnotations']
 			for j in range(len(labels)):
 				word = lmtzr.lemmatize(labels[j]['description'])
-				label_output.append(word.encode('utf8').lower())
-
+				label_output.append(word.lower())
+		
 			if len(label_output) > 0:
 				fh.write(fname + ':' + ','.join(label_output) + '\n')
-	return 0
+	except:
+		with open(os.path.join(cwd, 'exceptions.txt'), 'a') as f:
+			f.write(fname + '\n')
+
+	return label_output
 
 if __name__ == '__main__':
-	img_dir = os.path.join(cwd, 'Original_Image')
-	os.chdir(img_dir)
-	fnames = glob('*')
-	get_top_objects(img_dir, fnames)
+	with ProcessPoolExecutor() as executor, open('Board_Image_Objects.txt', 'a') as fh:
+		os.chdir(img_dir)
+		fnames = glob('*')[:24930]
+		# print(fnames[0])
+		# get_top_objects(fnames)
+		for fname, label_output in zip(fnames, executor.map(get_top_objects, fnames)):
+			fh.write(fname + ':' + ','.join(label_output) + '\n')
+			print(fname)
